d2e57806ba9cc202d9dfaebd6fce1fbe74b08531
hackassin <amlan.ultimate@gmail.com>
2019-07-06 09:52:17 +0530

    Explored enumeration and implicit indexing with for loop

---
+++ Just Basics/enumerate.py
@@ -0,0 +1,7 @@
+alphas = [0.5, 1e2, 1e3]
+
+for ii, alpha in enumerate(alphas):
+    print(ii, alpha)
+print(enumerate(alphas))
+
+'''The term "ii" happens to be the index. And indexing starts from 0'''
